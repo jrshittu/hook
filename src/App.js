2e782ec12fc2f3e6639ca799a0ebd018f08e38cb
Abideen <misterjrshittu@gmail.com>
2023-08-06 07:45:15 +0100

    added axios and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Container, Typography } from '@mui/material';
+import TodoList from './components/TodoList';
+// import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  const [ todos, setTodos ] = useState([
+    {id: 1, text: 'Buy Groceries', completed: false},
+    {id: 2, text: 'Clean the house', completed: false},
+    {id: 3, text: 'Do dishes', completed: false},
+    {id: 4, text: 'Do Laundry', completed: false}
+  ]);
+  
+  
   return (
-    <div>
-    
-    </div>
-  );
+    <Container maxWidth='sm'>
+      <Typography variant='h4' align='center' gutterBottom mt= '24px' color="red">
+        Hook App
+      </Typography>
+      <TodoList todos={todos} />
+    </Container>
+  )
 }
 
 export default App;
