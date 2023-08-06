6f52d689336ecff5d5b1b8dfd35544f43c19bc59
Abideen <misterjrshittu@gmail.com>
2023-08-06 15:55:07 +0100

    Finished the BareApp

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Container, Typography } from '@mui/material';
 import TodoList from './components/TodoList';
+import AddTodoForm from './components/AddTodoForm';
 // import axios from 'axios';
 
 const App = () => {
 
-  const [ todos, setTodos ] = useState([
-    {id: 1, text: 'Buy Groceries', completed: false},
-    {id: 2, text: 'Clean the house', completed: false},
-    {id: 3, text: 'Do dishes', completed: false},
-    {id: 4, text: 'Do Laundry', completed: false}
-  ]);
+  const [ todos, setTodos ] = useState([]);
 
-  // put useEffect here
-  
+  const handleAddTodo = (task) => {
+    const newTodo = { id:Date.now(), text: task, completed: false };
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  }
+
+  const handleDeleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
   
   return (
-    <Container maxWidth='sm' sx={{backgroundColor: "yellow", }}>
+    <Container maxWidth='sm'>
       <Typography variant='h4' align='center' gutterBottom mt= '24px' color="red" paddingTop="10px">
         Hook App
       </Typography>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onDelete={handleDeleteTodo}/>
+      <AddTodoForm todos={todos} onAdd={handleAddTodo}/>
     </Container>
   )
 }
